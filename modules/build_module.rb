8cea5cb04c6eb90fa66605fa2304bb4204476b49
Alex Antonyuk <alex@antonyuk.me>
2013-01-08 09:57:46 +0200

    added config option "project name"

--- modules/build_module.rb
+++ modules/build_module.rb
@@ -9,7 +9,7 @@ module BuildModule
       ## build workspace
       system "xcodebuild -workspace \"#{runner.config['workspace']['name']}.xcworkspace\" -scheme \"#{runner.config['workspace']['scheme']}\" -configuration \"#{runner.config['build']['configuration']}\" -sdk \"#{runner.config['build']['sdk']}\" CONFIGURATION_BUILD_DIR=\"#{runner.config['run']['build_dir']}\" clean build" or fail "Build failed"
     else
-      system "xcodebuild -configuration \"#{runner.config['build']['configuration']}\" -sdk \"#{runner.config['build']['sdk']}\" -target \"#{runner.config['project']['target']}\" clean build CONFIGURATION_BUILD_DIR=\"#{runner.config['run']['build_dir']}\"" or fail "Build failed"
+      system "xcodebuild -project \"#{runner.config['project']['name']}.xcodeproj\" -configuration \"#{runner.config['build']['configuration']}\" -sdk \"#{runner.config['build']['sdk']}\" -target \"#{runner.config['project']['target']}\" clean build CONFIGURATION_BUILD_DIR=\"#{runner.config['run']['build_dir']}\"" or fail "Build failed"
     end
     
     true
