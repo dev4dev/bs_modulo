99cd8c14062fcaba192b9ba8036104b8fcfe7272
Alex Antonyuk <alex@antonyuk.me>
2013-01-14 14:00:14 +0200

    added module for copying build file to specified dir

---
+++ modules/copy_ipa_module.rb
@@ -0,0 +1,18 @@
+
+module CopyIpaModule
+  extend self
+  
+  def run runner
+    unless runner.config['copy_ipa']['enabled']
+      puts 'skipping...'
+      return true
+    end
+    
+    ipa_file = runner.config['runtime']['ipa_file']
+    output_dir = runner.config['copy_ipa']['output_dir']
+    FileUtils.cp ipa_file, output_dir
+    
+    true
+  end
+  
+end
