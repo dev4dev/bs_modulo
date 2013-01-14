35df7d592be6900f1ed7c380690a38bc768b16d6
Alex Antonyuk <alex@antonyuk.me>
2013-01-14 11:59:32 +0200

    fixed dsym_pack module

--- modules/pack_dsym_module.rb
+++ modules/pack_dsym_module.rb
@@ -10,12 +10,12 @@ module PackDsymModule
     
     puts 'Packing dSYMs...'
 
-    ipa_dir = File.expand_path(runner.config['ipa_dir']) + '/'
+    output_dir = File.expand_path(runner.config['pack_dsym']['output_dir']) + '/'
     FileUtils.cd(runner.config['runtime']['build_dir']) do
       dsym_file = "#{runner.config['runtime']['output_file_name']}.dSYM.zip"
       system "zip -r \"#{dsym_file}\" \"#{runner.config['runtime']['app_file_name']}.app.dSYM\""
-      rm_f "#{ipa_dir}#{dsym_file}" or fail "failed to remove dSYM in #{ipa_dir}"
-      cp dsym_file, ipa_dir
+      rm_f "#{output_dir}#{dsym_file}" or fail "failed to remove dSYM in #{output_dir}"
+      cp dsym_file, output_dir
       rm_f dsym_file
     end
     
