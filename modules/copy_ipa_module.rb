c395194a8dffa93599a0e18d07b244c67c51ea49
Alex Antonyuk <alex@antonyuk.me>
2013-01-14 14:09:43 +0200

    fixed copy_ipa module, expand full path for output dir

--- modules/copy_ipa_module.rb
+++ modules/copy_ipa_module.rb
@@ -9,7 +9,7 @@ module CopyIpaModule
     end
     
     ipa_file = runner.config['runtime']['ipa_file']
-    output_dir = runner.config['copy_ipa']['output_dir']
+    output_dir = real_dir runner.config['copy_ipa']['output_dir']
     FileUtils.cp ipa_file, output_dir
     
     true
