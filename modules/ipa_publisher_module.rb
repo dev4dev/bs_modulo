54ecefb5295ca3c32ae7895bbe5d664be64e1b04
Alex Antonyuk <alex@antonyuk.me>
2013-01-15 09:51:43 +0200

    fixed bug in ipa_publisher module

--- modules/ipa_publisher_module.rb
+++ modules/ipa_publisher_module.rb
@@ -10,13 +10,13 @@ module IpaPublisherModule
     
     puts "Publishing IPA..."
 
+    template_path = real_dir runner.config['ipa_publisher']['template']
     FileUtils.cd(runner.config['runtime']['build_dir']) do
       ipa_file = runner.config['runtime']['ipa_file']
       tmp_dir = real_dir './tmp/'
       FileUtils.mkdir_p tmp_dir
-      template_path = real_dir runner.config['ipa_publisher']['template']
       system %Q[/usr/local/bin/IpaPublisher -ipaPath #{ipa_file} -o #{tmp_dir} -htmlPath #{template_path} -URL #{runner.config['ipa_publisher']['web_path']}]
-      system %Q[scp -r "#{tmp_dir}*" "#{runner.config['ipa_publisher']['fs_path']}"]
+      system %Q[scp -r #{tmp_dir}* #{runner.config['ipa_publisher']['fs_path']}]
       FileUtils.rm_rf tmp_dir, {:secure => true}
     end
     
