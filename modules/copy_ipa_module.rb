d0fc7558d9adc14a428ff7508b87d33eae969bb2
Alex Antonyuk <alex@antonyuk.me>
2013-01-15 17:22:51 +0200

    fixed rm method

--- modules/copy_ipa_module.rb
+++ modules/copy_ipa_module.rb
@@ -11,7 +11,7 @@ module CopyIpaModule
     output_dir = real_dir runner.config['copy_ipa']['output_dir']
     if runner.config['copy_ipa']['clear_old']
       puts "Removing #{output_dir}#{runner.config['runtime']['output_file_mask']}..."
-      rm_f output_dir + runner.config['runtime']['output_file_mask']
+      rm_rf output_dir + runner.config['runtime']['output_file_mask']
     end
     
     ipa_file = runner.config['runtime']['ipa_file']
