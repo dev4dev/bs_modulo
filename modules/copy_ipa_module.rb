a8f8430de0b9b9bc44b9d2ab57462595a996fd87
Alex Antonyuk <alex@antonyuk.me>
2013-01-15 16:49:42 +0200

    added ability to remove old builds in copy ipa module

--- modules/copy_ipa_module.rb
+++ modules/copy_ipa_module.rb
@@ -8,8 +8,14 @@ module CopyIpaModule
       return true
     end
     
-    ipa_file = runner.config['runtime']['ipa_file']
     output_dir = real_dir runner.config['copy_ipa']['output_dir']
+    if runner.config['copy_ipa']['clear_old']
+      FileUtils.cd output_dir do
+        rm_f runner.config['runtim']['output_file_mask']
+      end
+    end
+    
+    ipa_file = runner.config['runtime']['ipa_file']
     FileUtils.cp ipa_file, output_dir
     
     true
--- modules/pack_ipa_module.rb
+++ modules/pack_ipa_module.rb
@@ -7,6 +7,7 @@ module PackIpaModule
     
     output_file_name = runner.config['prefix'] ?  runner.config['prefix'] + '_' : ''
     output_file_name += "#{runner.config['runtime']['app_file_name']}_#{runner.config['branch']['name']}_#{runner.config['build']['configuration']}"
+    runner.config['runtime']['output_file_mask'] = "#{output_file_name}*"
     output_file_name += runner.config['runtime']['version'] ? '_v' + runner.config['runtime']['version'] : ''
     runner.config['runtime']['output_file_name'] = output_file_name
     
