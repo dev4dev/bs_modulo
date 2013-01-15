3ff93cb7f2204e5b4f73f725a37f5595096b1336
Alex Antonyuk <alex@antonyuk.me>
2013-01-15 14:05:54 +0200

    use prefix config property to add prefix for output IPA filename if needed

--- modules/pack_ipa_module.rb
+++ modules/pack_ipa_module.rb
@@ -5,7 +5,8 @@ module PackIpaModule
   def run runner
     puts 'Packing IPA...'
     
-    output_file_name = "#{runner.config['runtime']['app_file_name']}_#{runner.config['branch']['name']}_#{runner.config['build']['configuration']}"
+    prefix = runner.config['prefix'] ?  runner.config['prefix'] + '_' : ''
+    output_file_name = "#{prefix}#{runner.config['runtime']['app_file_name']}_#{runner.config['branch']['name']}_#{runner.config['build']['configuration']}"
     runner.config['runtime']['output_file_name'] = output_file_name
     ipa_file = "#{output_file_name}.ipa"
     runner.config['runtime']['ipa_file'] = runner.config['runtime']['build_dir'] + ipa_file
