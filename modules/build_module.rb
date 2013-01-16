c68ad5b679d5d70a0524f6a6dbc4f68667efb026
Alex Antonyuk <alex@antonyuk.me>
2013-01-16 13:58:32 +0200

    refactored build module

--- modules/build_module.rb
+++ modules/build_module.rb
@@ -8,12 +8,22 @@ module BuildModule
     profile_file = real_file runner.config.profile.file
     build_profile = real_file '~/Library/MobileDevice/Provisioning Profiles/build.mobileprovision'
     FileUtils.cp profile_file, build_profile
+    build_parameters = [
+      %Q[-configuration "#{runner.config.build.configuration}"],
+      %Q[-sdk "#{runner.config.build.sdk}"],
+      %Q[CONFIGURATION_BUILD_DIR="#{runner.config.runtime.build_dir}"],
+      %Q[clean],
+      %Q[build]
+    ]
     if runner.config.using_pods?
       ## build workspace
-      result = system %Q[xcodebuild -workspace "#{runner.config.workspace.name}.xcworkspace" -scheme "#{runner.config.workspace.scheme}" -configuration "#{runner.config.build.configuration}" -sdk "#{runner.config.build.sdk}" CONFIGURATION_BUILD_DIR="#{runner.config.runtime.build_dir}" clean build]
+      build_parameters.unshift %Q[-workspace "#{runner.config.workspace.name}.xcworkspace"]
+      build_parameters.unshift %Q[-scheme "#{runner.config.workspace.scheme}"]
     else
-      result = system %Q[xcodebuild -project "#{runner.config.project.name}.xcodeproj" -target "#{runner.config.project.target}" -configuration "#{runner.config.build.configuration}" -sdk "#{runner.config.build.sdk}" CONFIGURATION_BUILD_DIR="#{runner.config.runtime.build_dir}" clean build]
+      build_parameters.unshift %Q[-project "#{runner.config.project.name}.xcodeproj"]
+      build_parameters.unshift %Q[-target "#{runner.config.project.target}"]
     end
+    result = system %Q[xcodebuild #{build_parameters.join(' ')}]
     rm_f build_profile
     unless result
        fail "Build failed"
