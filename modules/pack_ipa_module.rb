0a83b7447c0c5c044e1bbf1853c9e331c3a2e6ab
Alex Antonyuk <alex@antonyuk.me>
2013-01-16 14:39:43 +0200

    reworked pack_ipa module

--- modules/pack_ipa_module.rb
+++ modules/pack_ipa_module.rb
@@ -3,8 +3,6 @@ module PackIpaModule
   extend self
   
   def run runner
-    puts 'Packing IPA...'
-    
     output_file_name = runner.config.naming.prefix ?  runner.config.naming.prefix + '_' : ''
     output_file_name += "#{runner.config.runtime.app_file_name}_#{runner.config.branch.name}_#{runner.config.build.configuration}"
     runner.config.runtime.output_file_mask = "#{output_file_name}*"
@@ -18,22 +16,8 @@ module PackIpaModule
     runner.config.runtime.ipa_file = runner.config.runtime.build_dir + ipa_file
     
     app_name = runner.config.runtime.app_file_name
-    FileUtils.cd(runner.config.runtime.build_dir) do
-      rm_rf 'Payload'
-      rm_f "*.ipa"
-      FileUtils.mkdir_p 'Payload/Payload'
-      FileUtils.cp_r("#{app_name}.app", 'Payload/Payload', {:preserve => true})
-      if File.exists? "#{app_name}.app/iTunesArtwork"
-         cp "#{app_name}.app/iTunesArtwork", 'Payload/iTunesArtwork'
-      end
-      system %Q[ditto -c -k Payload "#{ipa_file}"]
-      
-      rm_rf "Payload"
-    end
-    
-    puts 'Signing IPA...'
-    
     if runner.config.profile.identity
+      puts 'Packing & Signing IPA...'
       sdk = runner.config.build.sdk
       app_file = app_name + ".app"
       identity = runner.config.profile.identity
@@ -41,6 +25,20 @@ module PackIpaModule
       ipa_output_path = runner.config.runtime.build_dir + ipa_file
       FileUtils.cd(runner.config.runtime.build_dir) do
         system %Q[xcrun -sdk "#{sdk}" PackageApplication -v "#{app_file}" -o "#{ipa_output_path}" --sign "#{identity}" --embed "#{profile_file}"] or fail "Failed xcrun packaging and signing ipa"
+      end
+    else
+      # pack ipa manually
+      FileUtils.cd(runner.config.runtime.build_dir) do
+        puts 'Packing IPA...'
+        rm_rf 'Payload'
+        rm_f "*.ipa"
+        FileUtils.mkdir_p 'Payload/Payload'
+        FileUtils.cp_r("#{app_name}.app", 'Payload/Payload', {:preserve => true})
+        if File.exists? "#{app_name}.app/iTunesArtwork"
+           cp "#{app_name}.app/iTunesArtwork", 'Payload/iTunesArtwork'
+        end
+        system %Q[ditto -c -k Payload "#{ipa_file}"]
+        rm_rf "Payload"
       end
     end
     
