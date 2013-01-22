8387b22e75b120c3dc04222bb097b31042b5e58e
Alex Antonyuk <alex@antonyuk.me>
2013-01-22 14:40:06 +0200

    store dsym file path in runtime config section

--- modules/pack_dsym_module.rb
+++ modules/pack_dsym_module.rb
@@ -13,6 +13,7 @@ module PackDsymModule
     output_dir = real_dir runner.config.pack_dsym.output_dir
     FileUtils.cd(runner.config.runtime.build_dir) do
       dsym_file = "#{runner.config.runtime.output_file_name}.dSYM.zip"
+      runner.config.runtime.dsym_file = runner.config.runtime.build_dir + dsym_file
       system %Q[zip -r "#{dsym_file}" "#{runner.config.runtime.app_file_name}.app.dSYM"]
       rm_f "#{output_dir}#{dsym_file}" or fail "failed to remove dSYM in #{output_dir}"
       cp dsym_file, output_dir
