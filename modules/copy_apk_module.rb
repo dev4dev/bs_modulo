28f93376243a6d0a9b6b1c6a429cca89b91fa1e7
Alex Antonyuk <alex@antonyuk.me>
2013-02-01 16:03:01 +0200

    improved copy_apk module

--- modules/copy_apk_module.rb
+++ modules/copy_apk_module.rb
@@ -7,8 +7,27 @@ module CopyApkModule
       return true
     end
     
+    FileUtils.cd 'bin' do
+      apks = Dir.glob(runner.config.runtime.app_name + '-' + runner.config.build_android.configuration + '*.apk')
+      apk_file = apks.first unless apks.empty?
+      if apk_file
+        runner.config.runtime.apk_file_glob = runner.config.runtime.project_dir + 'bin/' + apk_file
+      end
+    end
+    
+    dest_file_name = runner.config.copy_apk.naming.prefix \
+      ? runner.config.copy_apk.naming.prefix \
+      : runner.config.runtime.app_name
+    ver = AndroidVersion.new 'AndroidManifest.xml'
+    dest_file_name += "_#{runner.config.branch.name}_#{runner.config.runtime.configuration}_v#{ver.version_name}.apk"
+    puts dest_file_name
     output_dir = real_dir runner.config.copy_apk.output_dir
-    cp runner.config.runtime.apk_file, output_dir
+    output_file_path = output_dir + dest_file_name
+    if File.exists? runner.config.runtime.apk_file
+      cp runner.config.runtime.apk_file, output_file_path
+    else
+      cp runner.config.runtime.apk_file_glob, output_file_path
+    end
     
     true
   end
