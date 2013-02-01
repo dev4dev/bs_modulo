9eaa4527865f90e271f4ffa50d21884568201157
Alex Antonyuk <alex@antonyuk.me>
2013-02-01 15:58:26 +0200

    added parameter for pushing bumped version to repo

--- modules/bump_version_module.rb
+++ modules/bump_version_module.rb
@@ -13,9 +13,11 @@ module BumpVersionModule
     system %Q[agvtool bump -all]
     version_number = `agvtool vers -terse`.strip
     system %Q[agvtool new-marketing-version "#{version_number}"]
-    puts "Push updated version numbers to git"
-    system %Q[git commit -am "AUTOBUILD -- configuration: #{runner.config.runtime.configuration}, ver: #{version_number}"]
-    system %Q[git push origin #{runner.config.branch.name}]
+    if runner.config.bump_version.push?
+      puts "Push updated version numbers to git"
+      system %Q[git commit -am "AUTOBUILD -- configuration: #{runner.config.runtime.configuration}, ver: #{version_number}"]
+      system %Q[git push origin #{runner.config.branch.name}]
+    end
     
     true
   end
