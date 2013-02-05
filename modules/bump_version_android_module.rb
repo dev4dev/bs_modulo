1c93017bae6fe6ead585dcde09a536b1b5d7bc85
Alex Antonyuk <alex@antonyuk.me>
2013-02-05 17:24:19 +0200

    fixed bug in bump_version_android module, when it saves manifest file after git commit, and it miss version update in repo

--- modules/bump_version_android_module.rb
+++ modules/bump_version_android_module.rb
@@ -14,13 +14,13 @@ module BumpVersionAndroidModule
     begin
       ver = AndroidVersion.new manifest_xml_file
       ver.increment
+      ver.write
       puts "Bumping to versionCode:#{ver.version_code} versionName:#{ver.version_name}"
       if config.bump_version_android.push?
         puts "Push updated version numbers to git"
         system %Q[git commit -am "AUTOBUILD -- configuration: #{config.runtime.configuration}, versionCode:#{ver.version_code} versionName:#{ver.version_name}"]
         system %Q[git push origin #{config.branch.name}]
       end
-      ver.write
     rescue e
       fail 'Error bumping version'
     end
