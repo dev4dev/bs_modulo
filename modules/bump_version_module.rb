e566834810e47196ae550fbe96d42d246624d0b1
Alex Antonyuk <alex@antonyuk.me>
2013-02-07 16:40:09 +0200

    added option for bump_version module to choose style for marketing version update

--- modules/bump_version_module.rb
+++ modules/bump_version_module.rb
@@ -11,11 +11,20 @@ module BumpVersionModule
     puts "Bumping version..."
     
     system %Q[agvtool bump -all]
-    version_number = `agvtool vers -terse`.strip
-    system %Q[agvtool new-marketing-version "#{version_number}"]
+    build_number = `agvtool vers -terse`.strip
+    
+    version = build_number
+    if config.bump_version.nice_mver?
+      mver = `agvtool mvers -terse1`.strip.split '.'
+      (3 - mver.count).times {mver << 0}
+      mver[2] = build_number
+      version = mver.join '.'
+    end
+    system %Q[agvtool new-marketing-version "#{version}"]
+    
     if config.bump_version.push?
       puts "Push updated version numbers to git"
-      system %Q[git commit -am "AUTOBUILD -- configuration: #{config.runtime.configuration}, ver: #{version_number}"]
+      system %Q[git commit -am "AUTOBUILD -- configuration: #{config.runtime.configuration}, ver: #{version}"]
       system %Q[git push origin #{config.branch.name}]
     end
     
