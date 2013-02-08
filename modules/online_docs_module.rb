c7711f201512e2bfacb84a52e6426a82e7c25bd3
Alex Antonyuk <alex@antonyuk.me>
2013-02-08 16:40:28 +0200

    changed paths workaround in online_docs module, you have to specify full fs and web paths in config

--- modules/online_docs_module.rb
+++ modules/online_docs_module.rb
@@ -10,11 +10,9 @@ module OnlineDocsModule
     
     system %Q[/usr/local/bin/appledoc --output ./web_docs --no-create-docset]
     mv './web_docs/html', './web_docs/docs'
-    path_prefix = "web_docs/#{config.runtime['configuration']}"
-    system %Q[scp -r ./web_docs/docs/* #{config.online_docs.fs_path}#{path_prefix}]
+    system %Q[scp -r ./web_docs/docs/* #{config.online_docs.fs_path}]
     rm_rf './web_docs/' or fail 'to clean docs directory'
-    webserver_dir = config.online_docs.web_path + path_prefix
-    puts "Documentation updated #{webserver_dir}"
+    puts "Documentation updated #{config.online_docs.web_path}"
     
     true
   end
