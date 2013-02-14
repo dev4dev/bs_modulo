bbce794ef3a86a2db5aaeb3b096092b7569d441f
Alex Antonyuk <alex@antonyuk.me>
2013-02-14 14:43:44 +0200

    converted consts to vars in docs_module to remove errors

--- modules/docs_module.rb
+++ modules/docs_module.rb
@@ -9,24 +9,24 @@ module DocsModule
     end
     
     # docs
-    OFFICE_PATH = "/Applications/OpenOffice.org.app/Contents/MacOS/"
+    office_path = "/Applications/OpenOffice.org.app/Contents/MacOS/"
     fail 'There is no Office' unless File.exists? OFFICE_PATH
     
-    SOFFICE = OFFICE_PATH + 'soffice'
-    UNOPKG = OFFICE_PATH + 'unopkg'
+    soffice = office_path + 'soffice'
+    unopkg = office_path + 'unopkg'
     FileUtils.cd 'docs' do
       # # add OXM lib extension in OpenOffice
-      system %Q[#{UNOPKG} add --shared #{docs}#{config.docs.ext}] if config.docs.ext
+      system %Q[#{unopkg} add --shared #{docs}#{config.docs.ext}] if config.docs.ext
       
       config.docs.files.each do |doc_file|
         # pdf
         if config.docs.formats.pdf?
-          system %Q[#{SOFFICE} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToPDF(#{doc_file})"] or fail "Convert to PDF failed"
+          system %Q[#{soffice} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToPDF(#{doc_file})"] or fail "Convert to PDF failed"
         end
         
         #html
         if config.docs.formats.html?
-          system %Q[#{SOFFICE} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToHTML(#{doc_file})"] or fail "Convert to HTML failed"
+          system %Q[#{soffice} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToHTML(#{doc_file})"] or fail "Convert to HTML failed"
         end
       end
     end
