af1886803537d4667e0b92da8e7698aab503c57e
Alex Antonyuk <alex@antonyuk.me>
2013-02-14 16:46:19 +0200

    added logging messages to docs_module

--- modules/docs_module.rb
+++ modules/docs_module.rb
@@ -22,11 +22,13 @@ module DocsModule
       config.docs.files.each do |doc_file|
         # pdf
         if config.docs.formats.pdf?
+          puts "Converting #{doc_file} to PDF..."
           system %Q[#{soffice} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToPDF(#{doc_file})"] or fail "Convert to PDF failed"
         end
         
         #html
         if config.docs.formats.html?
+          puts "Converting #{doc_file} to HTML..."
           system %Q[#{soffice} -invisible "macro:///OXMLibrary.OXMExport.ConvertDocToHTML(#{doc_file})"] or fail "Convert to HTML failed"
         end
       end
