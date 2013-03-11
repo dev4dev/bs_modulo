84edc45d4e86ac960237c05ead3b6214a209a057
Alex Antonyuk <alex@antonyuk.me>
2013-03-11 10:44:18 +0200

    added ant.properties file updates with values from config file in build_android module

--- modules/build_android_module.rb
+++ modules/build_android_module.rb
@@ -4,6 +4,26 @@ class BuildAndroidModule < BaseModule
   
   def self.run config
     puts 'Building project...'
+    
+    ## update ant.properties
+    properties_file = sysconf.android_project_properties_file
+    properties = YAML.load_file(properties_file) if File.exists? properties_file
+    props = properties[config.build_android.properties_key] if properties
+    
+    if props
+      puts 'Updating ant.properties file...'
+      project_props_file = config.runtime.project_dir + 'ant.properties'
+      project_props = nil
+      if File.exists? project_props_file
+        project_props = Properties.load_from_file project_props_file
+      else
+        project_props = Properties.new []
+      end
+      project_props.set props
+      project_props.save_to_file project_props_file
+    end
+    
+    ## build
     system %Q[ant #{config.build_android.configuration}] or fail "build project"
   end
 end
