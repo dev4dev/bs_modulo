13d6564cf39357627402c02e9b2153e0bbca2e58
Alex Antonyuk <alex@antonyuk.me>
2013-03-14 11:59:13 +0200

    fixed code formatting

--- modules/tests_module.rb
+++ modules/tests_module.rb
@@ -5,7 +5,7 @@ class TestsModule < BaseModule
   
   def self.run config
     puts 'Running tests...'
-	system %Q[killall -m -KILL "iPhone Simulator"]
+    system %Q[killall -m -KILL "iPhone Simulator"]
     result = system %Q[xcodebuild -target #{config.tests.target} -configuration Debug -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build 2>&1 | ocunit2junit]
     unless result
       fail "Unit tests failed"
