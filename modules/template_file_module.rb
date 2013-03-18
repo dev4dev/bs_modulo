dc5e1a13a541ed0895ca760937b9e9ea8bdbfe40
Alex Antonyuk <alex@antonyuk.me>
2013-03-18 15:49:50 +0200

    template_file module improvements

--- modules/template_file_module.rb
+++ modules/template_file_module.rb
@@ -7,14 +7,18 @@ class TemplateFileModule < BaseModule
     files = config.template_file.files
     if files
       files.each do |file|
-        File.open(file[:from], 'r') do |input_file|
-          puts "copy #{file[:from]} to #{file[:to]} with #{file[:vars]}"
-          data = input_file.read
-          file[:vars].each {|from, to| data.gsub! %Q[{#{from.to_s}}], to}
-          File.open(file[:to], 'w') do |output_file|
-            output_file << data
+        if File.exists? real_file(file[:from])
+          File.open(real_file(file[:from]), 'r') do |input_file|
+            puts "Copy #{file[:from]} to #{file[:to]} with #{file[:vars]}"
+            data = input_file.read
+            file[:vars].each {|from, to| data.gsub! %Q[{#{from.to_s}}], to}
+            File.open(real_file(file[:to]), 'w') do |output_file|
+              output_file << data
+            end
           end
-        end if File.exists? file[:from]
+        else
+          puts %Q[Warning: Template file does not exist "#{file[:from]}"...]
+        end
       end
     end
   end
