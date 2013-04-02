e75d037f34a3e54449a251fc7e817d21028cdb70
Alex Antonyuk <alex@antonyuk.me>
2013-04-02 16:40:36 +0300

    added workspace support to tests module

--- modules/tests_module.rb
+++ modules/tests_module.rb
@@ -6,7 +6,17 @@ class TestsModule < BaseModule
   def self.run config
     info 'Running tests...'
     system %Q[killall -m -KILL "iPhone Simulator"]
-    result = system %Q[xcodebuild -target #{config.tests.target} -configuration Debug -sdk iphonesimulator TEST_AFTER_BUILD=YES clean build 2>&1 | ocunit2junit]
+    parameters = []
+    if config.tests.use_workspace
+      parameters << "-workspace #{config.tests.workspace.name}.xcworkspace"
+      parameters << "-scheme #{config.tests.workspace.scheme}"
+    else
+      parameters << "-target #{config.tests.target}"
+    end
+    parameters << "-configuration Debug"
+    parameters << "-sdk iphonesimulator"
+    parameters << "TEST_AFTER_BUILD=YES clean build 2>&1 | ocunit2junit"
+    result = system %Q[xcodebuild #{parameters.join(' ')}]
     unless result
       fail "Unit tests failed"
     end
