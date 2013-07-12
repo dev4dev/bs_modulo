25bfb306b5bb1cb69ed3af07f4041a43c9578187
Alex Antonyuk <alex@antonyuk.me>
2013-07-12 17:29:48 +0300

    install improvement

--- builder.rb
+++ builder.rb
@@ -16,12 +16,16 @@ command :install do |c|
   c.syntax = ' builder install'
   c.description = 'Install builder in system'
   bin_path = '/usr/local/bin/builder'
+  global_config_file = File.expand_path('~/.bs_modulo.yml')
   c.action do |args, options|
     if File.exist? bin_path
       fail "builder is already installed or its default name '#{bin_path}' is already taken by another app"
     else
       puts 'Installing...'
+      puts "...creating [#{bin_path}] symlink..."
       File.symlink __F__, '/usr/local/bin/builder'
+      puts "...creating global configuration file [#{global_config_file}]..."
+      open(global_config_file, "w") { |io|  io << ''} unless File.exist? global_config_file
       puts 'Done.'
     end
   end
