c6b906ee977d9914fb0cdac395390b1bcf5ef0c7
Alex Antonyuk <alex@antonyuk.me>
2013-09-17 11:52:26 +0300

    xcode_module: refactoring, added checking of current version

--- modules/xcode_module.rb
+++ modules/xcode_module.rb
@@ -9,6 +9,7 @@ class XcodeModule < BaseModule
     version = config.xcode.version
     version_path = sysconf.xcode[config.xcode.version]
     default_path = sysconf.xcode.def
+    current_path = `xcode-select -print-path`.chomp
     do_switch = version_path != default_path
     
     unless version_path
@@ -19,19 +20,24 @@ class XcodeModule < BaseModule
       unless File.exists? version_path
         fail "Xcode doesn't exist at #{version_path}"
       end
-      info "Switching Xcode version to #{version}"
-      result = system %Q{sudo xcode-select -switch "#{version_path}"}
-      fail "Switching Xcode to version #{version} failed" unless result
+      switch version, version_path
+    elsif default_path != current_path
+      switch "default", default_path
     else
       info "Xcode is already at required version"
     end
     
     hook :complete, proc {
       if do_switch
-        info "Switching Xcode back to default version"
-        result = system %Q{sudo xcode-select -switch "#{default_path}"}
-        fail "Switching Xcode to default version failed" unless result
+        switch "default", default_path
       end
     }
+  end
+  
+  private
+  def self.switch name, path
+    info "Switching Xcode back to #{name} version"
+    result = system %Q{sudo xcode-select -switch "#{path}"}
+    fail "Switching Xcode to #{name} version failed" unless result
   end
 end
